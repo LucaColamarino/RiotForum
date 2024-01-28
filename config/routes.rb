91f98b9a3d1ea3d36e155d61129311070419dd22
LucaColamarino <colamarino01@gmail.com>
2024-01-28 18:36:38 +0100

    Devise install

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'pages#home'
 
   get 'profile', to: 'pages#profile'
   get 'board', to: 'pages#board'
   get 'create_team', to: 'pages#create_team'
   get 'settings', to: 'pages#settings'
-
   
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
