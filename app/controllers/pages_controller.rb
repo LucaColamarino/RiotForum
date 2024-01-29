1f4a5212642e7c658d956e9279bad52ed46ca426
Stefano Moisuc <stefano.moisuc01@gmail.com>
2024-01-29 19:00:40 +0100

    Modificate pagine di home e scelta gioco(+css). Aggiunte pagina degli annunci e impostazioni. Aggiunta distinzione tra i 2 giochi (alcuni dettagli non si mantengono andando da una pagina all'altra)

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,14 @@
 class PagesController < ApplicationController
+  
+  @game = "LOL"; #per ora lo lascio come default, ma non va bene
+
+  def scelta
+  end
+
   def home
+   @game = params[:game];
+   @color = @game == "LOL"? "#483D8B" : "#dc3545 ";
+    #credo vada usata la sessione per mantenere l'info sul gioco scelto
   end
 
   def profile
@@ -13,4 +22,5 @@ class PagesController < ApplicationController
 
   def settings
   end
+
 end
