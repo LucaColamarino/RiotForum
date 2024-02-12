e1cb61d027ff9b1a04f743e51821f63b8eb02e43
andreapannu <pannunzioandrea@yahoo.it>
2024-02-12 19:32:55 +0100

    Messaggi 1 a 1 completi

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,6 @@ Rails.application.routes.draw do
   patch 'news/:id' , to: 'newpost#update'
   delete 'news/:id', to: 'newpost#destroy'
 
-  delete 'messages/:id', to: 'messages#destroy'
 
   resources :messages
 
