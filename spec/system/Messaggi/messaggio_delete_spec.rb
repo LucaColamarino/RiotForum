b4bbb42f3365b242a2109c05bf22665a89694124
andreapannu <pannunzioandrea@yahoo.it>
2024-02-19 00:55:04 +0100

    Implementata sicurezza nei messaggi e finita sicurezza nelle notizie

---
+++ spec/system/Messaggi/messaggio_delete_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe 'Crea Messaggio', type: :system do
+  let(:user) {create(:user)}
+  let(:user1) {create(:user)}
+
+  
+#  before(:each) do
+    # Creazione del messaggio prima di ogni scenario
+ #   @messaggio = create(:message)
+ # end
+
+
+  scenario 'Utente cancella messaggio' do
+    user1=create(:user1)
+    user=create(:user)
+    login_as(user, scope: :user)
+    @messaggio = create(:message)
+
+
+    visit 'messages/'+@messaggio.id.to_s
+
+
+    click_button "delete"
+    expect(page).to have_content('Messaggio cancellato con successo') 
+  end
+end
